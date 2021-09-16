c570c0dc6336a4ed6a2725ed0026d8815399ccf3
juliajykim <jkim24@gwu.edu>
2021-09-15 20:11:57 -0400

    add map class component

---
+++ frontend/components/Map/map_class.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import MarkerManager from "../../util/marker_manager";
+
+const mapOptions = {
+  center: { lat: 40.740879, lng: -73.987135 },
+  zoom: 13,
+  disableDefaultUI: true,
+  mapTypeControl: false,
+  streetViewControl: false,
+  scaleControl: false,
+  zoomConrol: false,
+  clickableIcons: false,
+  styles: [
+    {
+      featureType: "poi",
+      elementType: "labels",
+      stylers: [{ visibility: "off" }],
+    },
+  ],
+};
+
+class MapClass extends React.Component {
+  componentDidMount() {
+    this.map = new google.maps.Map(this.mapNode, mapOptions);
+    this.MarkerManager = new MarkerManager(
+      this.map
+      //   this.handleMarkerClick.bind(this)
+    );
+    this.registerListeners();
+    this.MarkerManager.updateMarkers(this.props.houses);
+  }
+
+  componentDidUpdate() {
+    this.MarkerManager.updateMarkers(this.props.houses);
+  }
+
+  registerListeners() {
+    const { dispatch, updateFilter } = this.props;
+    google.maps.event.addListener(this.map, "idle", () => {
+      const { north, south, east, west } = this.map.getBounds().toJSON();
+      const bounds = {
+        northEast: { lat: north, lng: east },
+        southWest: { lat: south, lng: west },
+      };
+      dispatch(updateFilter("bounds", bounds));
+    });
+  }
+
+  //   handleMarkerClick(property) {
+  //     if (property) {
+  //       this.props.openModal("property", property);
+  //     }
+  //   }
+
+  render() {
+    return <div id="map-container" ref={(map) => (this.mapNode = map)}></div>;
+  }
+}
+
+export default MapClass;
--- frontend/components/Search/search.jsx
+++ frontend/components/Search/search.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import { updateFilter } from "../../actions/filter_action";
 import HousesIndex from "../Houses/houses_index";
 import Map from "../Map/map";
+import MapClass from "../Map/map_class";
 
 const Search = (props) => {
+  const dispatch = useDispatch();
+  const houses = useSelector((state) => state.entities.houses);
   return (
     <div>
       <div>
@@ -11,7 +16,11 @@ const Search = (props) => {
       </div>
       <div className="listing-page">
         <div className="listing-page-left">
-          <Map />
+          <MapClass
+            dispatch={dispatch}
+            updateFilter={updateFilter}
+            houses={Object.values(houses)}
+          />
         </div>
         <div className="listing-page-right">
           <h1>Listings</h1>
